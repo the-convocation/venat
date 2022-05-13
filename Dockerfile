438e017d8d0605be23281f08d9cbf307b0e7ba02
Ava <hello@ava.dev>
2022-05-13 10:23:22 -0700

    feat: the barest of bones (#10)
    * feat: the barest of bones
    
    * feat: add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:16-alpine
+
+WORKDIR /usr/src/app
+
+COPY package.json .
+COPY yarn.lock .
+RUN yarn install
+
+COPY . .
+RUN yarn build
+
+CMD yarn start
---
+++ src/users/guards/user-is-bot-admin.guard.ts
@@ -0,0 +1,17 @@
+import { DiscordGuard, EventType } from '@discord-nestjs/core';
+import { UsersService } from '../users.service';
+import { Message, User } from 'discord.js';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserIsBotAdminGuard implements DiscordGuard {
+  constructor(private usersService: UsersService) {}
+
+  public async canActive(
+    event: EventType = 'interactionCreate',
+    [message, user]: [Message, User],
+  ): Promise<boolean> {
+    const userEntity = await this.usersService.find(user);
+    return userEntity && userEntity.isAdmin;
+  }
+}
