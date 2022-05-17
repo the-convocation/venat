373bb3aa06ade4a86d14bdfb99b43f14b07b0570
Ava Chaney <hello@ava.dev>
2022-05-16 21:56:30 -0700

    feat(sentry): add optional sentry support
    closes #63

--- core/src/app.module.ts
+++ core/src/app.module.ts
@@ -5,10 +5,12 @@ import { BotModule } from './bot/bot.module';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { ModuleLoader } from './module-system/module.loader';
+import { SentryModule } from './sentry/sentry.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    SentryModule,
     DatabaseModule,
     UsersModule,
     BotModule,
